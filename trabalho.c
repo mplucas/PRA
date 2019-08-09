03fad7fd59bf47790376261fbe33421f0cca6772
udesc <you@example.com>
2019-08-09 17:19:42 -0300

    Começada lógica para menu, ainda existe erro na opção de leitura de todos.

--- trabalho.c
+++ trabalho.c
@@ -9,16 +9,16 @@ typedef struct Registro{
   float valor;
 }registro;
 
-int main(int argc, char const *argv[]) {
+void writeFile(){
+
   int flag = 1;
-  FILE *fp;
   char simounao;
+  FILE *fp;
   registro r;
-  registro arq;
-  //121 bytes tamanho registro
 
   while (flag == 1) {
-    printf("Qual o nome?\n");
+
+    printf("Inserindo novo registro!\n\nQual o nome?\n");
     scanf("%s", r.nome);
     printf("Qual a marca?\n");
     scanf("%s", r.marca);
@@ -42,10 +42,31 @@ int main(int argc, char const *argv[]) {
       printf("Erro na escrita do arquivo\n");
     fclose(fp);
 
-    if((fp = fopen("arquivo.bin", "r")) == NULL)  {
-      printf("Erro na abertura do arquivo");
-      exit(1);
+    printf("Inserir outro registro? (S/N)\n");
+    scanf(" %c", &simounao);
+
+    if ((simounao == 'N') || (simounao == 'n')) {
+      flag = 0;
     }
+
+  }
+
+}
+
+void readFile(){
+
+  int i = 0;
+  FILE *fp;
+  registro arq;
+
+  if((fp = fopen("arquivo.bin", "r")) == NULL)  {
+    printf("Erro na abertura do arquivo");
+    exit(1);
+  }
+
+  while( !feof(fp) ){
+
+    printf("\nRNN: %i", i++);
 
     if(fread(&arq.nome, sizeof(char), 50,fp) != 50)
       printf("Erro na leitura do arquivo");
@@ -61,15 +82,40 @@ int main(int argc, char const *argv[]) {
 
     if(fread(&arq.valor, sizeof(float), 1,fp) != 1)
       printf("Erro na leitura do arquivo");
-    printf("\nO valor: %f", arq.valor);
-    fclose(fp);
+    printf("\nO valor: %f\n\n", arq.valor);
 
-    printf("Inserir outro registro? (S/N)\n");
-    scanf(" %c", &simounao);
-    if ((simounao == 'N') || (simounao == 'n')) {
-      flag = 0;
-    }
+    fseek(fp, 117, SEEK_CUR);
+
   }
+
+  fclose(fp);
+
+}
+
+int main(int argc, char const *argv[]) {
+  
+  int opc;
+  //117 bytes tamanho registro
+
+  do{
+
+    printf("PROGRAMA DE MANIPULAÇÃO DE ARQUIVO\n\n");
+    printf("Escolha a ação:\n 0 - Sair\n 1 - Inserir\n 2 - Ler Todos\n 3 - Ler por RNN\n");
+    scanf("%i", &opc);
+    
+    switch( opc ){
+      
+      case 1:
+        writeFile();
+      break;
+
+      case 2:
+        readFile();
+      break;
+
+    }
+
+  }while( opc != 0 );
 
   return 0;
 }
