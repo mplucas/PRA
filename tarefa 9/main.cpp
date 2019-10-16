b01fef2776d58ab509f11b7a152a532497011a00
lucas-m.p@hotmail.com <lucas-m.p@hotmail.com>
2019-10-16 11:55:32 -0300

    Terminado.

--- tarefa 9/main.cpp
+++ tarefa 9/main.cpp
@@ -4,11 +4,14 @@ using namespace std;
 
 #define TAM 500000
 
-int binSearch(int *v, int value){
+int binSearch(int *v, int value, int &numIterations){
 
     int first = 0, last = TAM, mid;
+    numIterations = 0;
 
     while( first <= last ){
+
+        numIterations++;
 
         mid = first + ((last - first) / 2);
 
@@ -27,9 +30,12 @@ int binSearch(int *v, int value){
 
 }
 
-int seqSearch(int *v, int value){
+int seqSearch(int *v, int value, int &numIterations){
 
+    numIterations = 0;
+    
     for(int i = 0; i < TAM; i++){
+        numIterations++;
         if( value == v[i] )
             return i;
     }
@@ -43,59 +49,76 @@ int main(int argc, const char** argv) {
     int v[TAM];
     
     clock_t Ticks[2];
-    Ticks[0] = clock();
-    Ticks[1] = clock();
     double Tempo;
+    int numIterations;
 
     for(int i = 0; i < 500000; i++)
         v[i] = i+1;
 
-    // for(auto i:v)
-    //     printf("%d\n",i);
-
-    printf("Buscando primeiro elemento(1):\n");
+    printf("\nBuscando primeiro elemento(1):\n");
 
     Ticks[0] = clock();
-    seqSearch(v,1);
+    seqSearch(v,1,numIterations);
     Ticks[1] = clock();
     Tempo = (double)((Ticks[1] - Ticks[0]) ) * 1000000.0/ CLOCKS_PER_SEC;
-    printf("Tempo gasto na busca sequencial: %g us.\n", Tempo);
+    printf("Busca sequencial:\nTempo gasto = %g us\tNumero de iteracoes = %i.\n", Tempo, numIterations);
 
     Ticks[0] = clock();
-    binSearch(v,1);
+    binSearch(v,1,numIterations);
     Ticks[1] = clock();
     Tempo = (double)((Ticks[1] - Ticks[0]) ) * 1000000.0/ CLOCKS_PER_SEC;
-    printf("Tempo gasto na busca binária: %g us.\n", Tempo);
+    printf("Busca Binaria:\nTempo gasto = %g us\tNumero de iteracoes = %i.\n", Tempo, numIterations);
 
-    printf("Buscando último elemento(500000):\n");
+    printf("\nBuscando último elemento(500000):\n");
 
     Ticks[0] = clock();
-    seqSearch(v,500000);
+    seqSearch(v,500000,numIterations);
     Ticks[1] = clock();
     Tempo = (double)((Ticks[1] - Ticks[0]) ) * 1000000.0/ CLOCKS_PER_SEC;
-    printf("Tempo gasto na busca sequencial: %g us.\n", Tempo);
+    printf("Busca sequencial:\nTempo gasto = %g us\tNumero de iteracoes = %i.\n", Tempo, numIterations);
 
     Ticks[0] = clock();
-    binSearch(v,500000);
+    binSearch(v,500000,numIterations);
     Ticks[1] = clock();
     Tempo = (double)((Ticks[1] - Ticks[0]) ) * 1000000.0/ CLOCKS_PER_SEC;
-    printf("Tempo gasto na busca binária: %g us.\n", Tempo);
+    printf("Busca Binaria:\nTempo gasto = %g us\tNumero de iteracoes = %i.\n", Tempo, numIterations);
 
-    printf("Buscando elemento mediano(250000):\n");
+    printf("\nBuscando elemento mediano(250000):\n");
 
     Ticks[0] = clock();
-    seqSearch(v,250000);
+    seqSearch(v,250000,numIterations);
     Ticks[1] = clock();
     Tempo = (double)((Ticks[1] - Ticks[0]) ) * 1000000.0/ CLOCKS_PER_SEC;
-    printf("Tempo gasto na busca sequencial: %g us.\n", Tempo);
+    printf("Busca sequencial:\nTempo gasto = %g us\tNumero de iteracoes = %i.\n", Tempo, numIterations);
 
     Ticks[0] = clock();
-    binSearch(v,250000);
+    binSearch(v,250000,numIterations);
     Ticks[1] = clock();
     Tempo = (double)((Ticks[1] - Ticks[0]) ) * 1000000.0/ CLOCKS_PER_SEC;
-    printf("Tempo gasto na busca binária: %g us.\n", Tempo);
+    printf("Busca Binaria:\nTempo gasto = %g us\tNumero de iteracoes = %i.\n", Tempo, numIterations);
 
-    //printf("%d %d\n", seqSearch(v,124999), binSearch(v,124999) );
+    printf("\nBuscando 200 elementos aleatorios:\n");
+    srand(time(NULL));
+    int acumIterations = 0;
+
+    Ticks[0] = clock();
+    for(int i = 0; i < 200; i++){
+        seqSearch(v, (rand() % 500000) + 1,numIterations);
+        acumIterations += numIterations;
+    }
+    Ticks[1] = clock();
+    Tempo = (double)((Ticks[1] - Ticks[0]) ) * 1000000.0/ CLOCKS_PER_SEC;
+    printf("Busca sequencial:\nTempo gasto = %g us\tNumero de iteracoes = %i.\n", Tempo, acumIterations);
+    acumIterations = 0;
+    
+    Ticks[0] = clock();
+    for(int i = 0; i < 200; i++){
+        binSearch(v, (rand() % 500000) + 1,numIterations);
+        acumIterations += numIterations;
+    }
+    Ticks[1] = clock();
+    Tempo = (double)((Ticks[1] - Ticks[0]) ) * 1000000.0/ CLOCKS_PER_SEC;
+    printf("Busca Binaria:\nTempo gasto = %g us\tNumero de iteracoes = %i.\n", Tempo, acumIterations);
 
     return 0;
 }
